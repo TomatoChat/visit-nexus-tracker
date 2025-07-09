13a396524e8497cdddb19d2e81f7b543299b8a34
Andrea <99046131+TomatoChat@users.noreply.github.com>
2025-07-09 20:09:12 +0200

    update file extension

--- src/hooks/use-roles.tsx
+++ src/hooks/use-roles.tsx
@@ -11,7 +11,7 @@ import {
   canViewAllVisits,
   type UserRole,
   type UserWithRole
-} from '../lib/roles.js';
+} from '../lib/roles';
 
 export function useRoles() {
   const [userRole, setUserRole] = useState<UserRole | null>(null);
